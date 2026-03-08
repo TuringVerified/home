4834c1508d844e588426553473016e3e9ed93d4e
Amay Agarwal <tt@turingtested.xyz>
2026-03-09 01:04:06 +0530

    Initial code for website

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/blogs")
+def blogs():
+    return render_template("blogs.html")
+
+@app.route("/pgp")
+def pgp():
+    return render_template("pgp.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory(app.static_folder, "robots.txt")
+
+if __name__ == "__main__":
+    app.run(debug=True)
